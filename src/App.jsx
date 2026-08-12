2917fd7dba2b14e75d080302a52b3869d8c2a58c
Cricketeer12345 <dhishant2009@gmail.com>
2026-08-12 16:35:25 -0500

    Add Analytics component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Learn from './pages/Learn'
@@ -15,6 +16,7 @@ function App() {
         <Route path="/people" element={<People />} />
         <Route path="/games" element={<Games />} />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   )
 }
